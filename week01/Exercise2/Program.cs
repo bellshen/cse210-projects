9d0290005b8e7b7043bb30e76667f1fc52922ab4
Bell Shen <bellshen@JoshuadeMacBook-Air.local>
2025-09-05 10:33:19 +0800

    exercise 2 upload

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.WriteLine("Enter your grade percentage:");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        string letter = "";
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Your passed!");
+        }
+        else
+        {
+            Console.WriteLine("Good luck next time.");
+        }
     }
 }
