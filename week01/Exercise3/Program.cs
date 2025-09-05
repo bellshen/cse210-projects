903a43d56f6ab3c5e985b5f687c6a1e4b89217cc
Bell Shen <bellshen@JoshuadeMacBook-Air.local>
2025-09-05 12:13:41 +0800

    exercise3 upload

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess;
+        do
+        {
+            Console.WriteLine("What is your guess? (1-100)");
+            string answer = Console.ReadLine();
+            guess = int.Parse(answer);
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+        } while (guess != magicNumber);
+
+        Console.WriteLine("You guess it!");
+            
+             
+        
+
+
     }
 }
