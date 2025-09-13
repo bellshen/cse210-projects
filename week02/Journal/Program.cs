ddc443385ab3db4f60ca1279b1fb5e82484a2b56
Bell Shen <bellshen@JoshuadeMacBook-Air.local>
2025-09-13 14:22:20 +0800

    first version

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,65 @@
 using System;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+
+        Journal journal = new Journal();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("What would you like to do?");
+            int choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                Entry entry = new Entry();
+                entry.Display();
+                journal.AddEntry(entry);
+            }
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("What is the filename?");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+
+            }
+            else if (choice == 4)
+            {
+                Console.WriteLine("What is the filename?");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+
+            }
+            else if (choice == 5)
+            {
+                Console.WriteLine("Quit!");
+                running = false;
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, please enter 1-5!");
+                
+            }   
+
+        }
+        
+
     }
 }
