06795cbf5470a113377860e4a4cbfcef1ff8c48a
Bell Shen <bellshen@JoshuadeMacBook-Air.local>
2025-09-19 16:36:49 +0800

    1st version

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections.Generic;
+
+//I added ScriptureLibrary.cs to get random scripture, 
+//it will be convinient for me add more scriptures.
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Console.Clear();
+        ScriptureLibrary library = new ScriptureLibrary();
+        Scripture scripture = library.GetRandomScripture();
+
+        while (scripture.IsCompletelyHidden() == false)
+        {
+            scripture.GetDisplayText();
+            Console.Write("Please Press Enter to hide more words or type 'quit' to end the program. ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(2);
+            Console.Clear();
+
+        }
+        Console.WriteLine("End of Program.");
+
     }
+
 }
