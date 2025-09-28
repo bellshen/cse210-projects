75504a3333c50bf8285c8c5b299d0700456e4301
Bell Shen <bellshen@JoshuadeMacBook-Air.local>
2025-09-28 10:33:43 +0800

    finished

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment a1 = new Assignment("Allan", "Gospel of Jesus Christ");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Julia", "Chapter 01","P103-180", "Intersections");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Ether", "History of War","The Cause of Second World War II in Asia Area");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
+
     }
 }
