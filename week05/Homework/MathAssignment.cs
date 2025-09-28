ba88142b2f7feae2ce2f93254ed73644ede47a98
Bell Shen <bellshen@JoshuadeMacBook-Air.local>
2025-09-28 10:32:49 +0800

    finished

---
+++ week05/Homework/MathAssignment.cs
@@ -0,0 +1,15 @@
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string name, string topic, string textbooksection, string problems): base(name, topic)
+    {
+        _textbookSection = textbooksection;
+        _problems = problems;
+    }
+    public string GetHomeworkList()
+    {
+        return $"TextBookSection: {_textbookSection}, Problem: {_problems}";
+    }
+}
