789c87ff3a64f0516378009abfbc2c885f43fd8b
Bell Shen <bellshen@JoshuadeMacBook-Air.local>
2025-10-09 19:42:04 +0800

    1st version

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,38 @@
+using System.Security.Cryptography.X509Certificates;
+
+public abstract class Goal
+{
+    protected string _shortName;
+    protected string _description;
+    protected int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+
+    }
+
+    public abstract void RecordEvent();
+
+    public abstract bool IsComplete();
+    public virtual string GetDetailsString()
+    {
+        return "";
+    }
+    public abstract string GetStringRepresentation();
+
+    public string GetGoalName()
+    {
+        return _shortName;
+    }
+    public string GetDescription()
+    {
+        return _description;
+    }
+    public int GetPoints()
+    {
+        return _points;
+    }
+}
